ff923f4f755bc7888bf38544717f8d320c52bdfc
igl-myrick <igl.myrick@gmail.com>
2024-03-06 14:58:17 -0800

    add setTurn prototype

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,14 @@
 function Lobby() {
-  this.player1 = { currentScore: 0, isTurn: true }
-  this.player2 = { currentScore: 0, isTurn: false }
+  this.player1 = { currentScore: 0, isTurn: true };
+  this.player2 = { currentScore: 0, isTurn: false };
 }
+
+Lobby.prototype.setTurn = function(player) {
+  if (player === "player1") {
+    this.player1.isTurn = true;
+    this.player2.isTurn = false;
+  } else if (player === "player2") {
+    this.player1.isTurn = false;
+    this.player2.isTurn = true;
+  }
+};
